c38673f69fea271748650a0e6c2b9500fe4dd26d
Max Anderson <maxwell.c.anderson@gmail.com>
2020-09-28 21:01:17 -0700

    Add documentation on how to customize Reflex

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ copyright = "2020, Reflexive Security"
 author = "ReflexiveSecurity"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
